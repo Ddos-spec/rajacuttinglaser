ed8000a900ec6d29a824db19f07037f9e843f5dc
Ddos-spec <setgraph69@gmail.com>
2025-07-27 21:02:36 +0700

    mari kita coba lagi

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SITE_URL: string;
+  readonly BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
